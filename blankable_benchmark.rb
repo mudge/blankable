241bc91fb87a0562e08d11b3e13754e5282a2fea
Paul Mucur <mudge@mudge.name>
2008-03-30 22:33:15 +0100

    Optimised by stopping as soon as a single value is found.
    Added a trivial benchmark to demonstrate the performance improvement
    between the previous inject-based algorithm and the new, faster (but uglier)
    algorithm.

--- blankable_benchmark.rb
+++ blankable_benchmark.rb
@@ -1,18 +1,15 @@
-# Ruby Blankable Mixin
-# A mixin to add a `values_blank?` method to objects to determine whether their
-# values are blank or not.
-# 
-# e.g.
-#  {:a => '', :b => {:c => nil}}.values_blank?  #=> true
-#  ['', nil, '', ''].values_blank?              #=> true
-# 
+#!/usr/bin/env ruby
+
+# Ruby Blankable Mixin Benchmark
 # See http://mucur.name/posts/ruby-blankable-mixin for more information.
 #
 # Copyright (c) Paul Mucur (http://mucur.name), 2008.
 # Licensed under the BSD License (LICENSE.txt).
 
+require 'benchmark'
+
 module Blankable
-  def values_blank?
+  def values_blank_loop?
     result = true
     for value in blankable_values
       result &&= if value.respond_to?(:values_blank?)
@@ -28,6 +25,20 @@ module Blankable
     end
     result
   end
+  
+  def values_blank_inject?
+    blankable_values.inject(true) do |result, value|
+      result && if value.respond_to?(:values_blank?)
+        value.values_blank?
+      elsif value.respond_to?(:blank?)
+        value.blank?
+      elsif value.respond_to?(:empty?)
+        value.empty?
+      else
+        value.nil?
+      end
+    end
+  end
 end
 
 class Hash
@@ -42,4 +53,14 @@ class Array
   def blankable_values
     self
   end
+end
+
+n = 10_000
+a = {}
+1000.times do
+  a[rand(1000)] = rand(1000)
+end
+Benchmark.bm do |x|
+  x.report("inject") { n.times { a.values_blank_inject? } }
+  x.report("loop  ") { n.times { a.values_blank_loop? } }
 end
